b4cbfc9ff0bfeff9c384bb2267b787e9cb59c299
vatharem <mayuriv2003@gmail.com>
2025-11-14 15:28:25 +0530

    Updated CORS for Vercel

--- src/main/java/com/portfolio/mayuri/Config/CorsConfig.java
+++ src/main/java/com/portfolio/mayuri/Config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.portfolio.mayuri.Config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -14,7 +13,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins(
+                                "https://my-portfolio-fjdr-j454jggyw-mayuris-projects-62260ff4.vercel.app",
+                                "http://localhost:3000"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
